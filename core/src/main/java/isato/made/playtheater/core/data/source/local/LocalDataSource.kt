7e65d146552f496c053de403fe1e168049e985fa
Isato <isato.iman@gmail.com>
2021-12-19 08:46:38 +0700

    update copyright

--- core/src/main/java/isato/made/playtheater/core/data/source/local/LocalDataSource.kt
+++ core/src/main/java/isato/made/playtheater/core/data/source/local/LocalDataSource.kt
@@ -1,3 +1,11 @@
+/*
+ * PlayTheater.core
+ * LocalDataSource.kt
+ * Created by Isato on 19/12/2021 8:43:11
+ * Source code available on: https://github.com/i-sato/PlayTheater
+ * Copyright (c) 2021. All rights reserved.
+ */
+
 package isato.made.playtheater.core.data.source.local
 
 import isato.made.playtheater.core.data.source.local.entity.GenreEntity
