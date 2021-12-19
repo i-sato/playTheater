c6a299a7e31d350d8088a8d0516d33b0ea3762e6
Isato <isato.iman@gmail.com>
2021-12-19 12:11:01 +0700

    add different view on landscape

--- favorite/src/main/java/isato/made/playtheater/favorite/ui/list/ListFavoriteFragment.kt
+++ favorite/src/main/java/isato/made/playtheater/favorite/ui/list/ListFavoriteFragment.kt
@@ -9,11 +9,13 @@
 package isato.made.playtheater.favorite.ui.list
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.gaelmarhic.quadrant.QuadrantConstants
 import dagger.hilt.android.EntryPointAccessors
@@ -61,7 +63,10 @@ class ListFavoriteFragment : Fragment() {
             }
 
             listFavoriteBinding?.rvFavorite?.apply {
-                layoutManager = LinearLayoutManager(context)
+                layoutManager = when (resources.configuration.orientation) {
+                    Configuration.ORIENTATION_LANDSCAPE -> GridLayoutManager(context, 2)
+                    else -> LinearLayoutManager(context)
+                }
                 adapter = movieAdapter
             }
 
